4ed17fe0db6a7bcbe7380f04fd5e3cfc64022d40
MKeinerrr <inagasaki10@gmail.com>
2026-08-30 15:46:49 -0500

    Semana 4
    * Semana 4
    
    ---------
    
    Co-authored-by: juliandmanjarrez-tech <juliandmanjarrez@gmail.com>
    Co-authored-by: diegobrr999-commits <diegobrr999@gmail.com>
    Co-authored-by: junior14700 <juniorjoseorozcoatencio@gmail.com>

---
+++ backend/tests/conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from app.core.database import engine, Base
+
+@pytest.fixture(scope="session", autouse=True)
+def crear_tablas():
+    # Crea todas las tablas definidas en los modelos antes de correr los tests
+    Base.metadata.create_all(bind=engine)
+    yield
+    # Opcional: elimina las tablas al terminar toda la sesión de tests
+    Base.metadata.drop_all(bind=engine)
